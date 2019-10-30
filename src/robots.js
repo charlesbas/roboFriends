0e31affa9b70b1b6fa22e6c07bd8631396776332
Charles Obasuyi <56266851+charlesbas@users.noreply.github.com>
2019-10-30 12:39:29 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Charles Obasuyi',
+    username: 'Charlesbas',
+    email: 'osayiobasuyi@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Tega Odiete',
+    username: 'MrTegz',
+    email: 'osayiobasuyi@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Samuel Adetula',
+    username: 'SaintSammie',
+    email: 'osayiobasuyi@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Daniel Adoghe',
+    username: 'Adogsman',
+    email: 'osayiobasuyi@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Fortune Ayeosa',
+    username: 'Charlesbas',
+    email: 'osayiobasuyi@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Kester Nero',
+    username: 'Charlesbas',
+    email: 'osayiobasuyi@gmail.com'
+  },
+]
